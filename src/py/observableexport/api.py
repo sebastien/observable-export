ed910d1db12199d8737ea56efc0e5303b5710107
Sébastien Pierre <sebastien.pierre@gmail.com>
2022-06-19 14:46:36 +1200

    [Update] api: refactored, added resolve

--- src/py/observableexport/api.py
+++ src/py/observableexport/api.py
@@ -1,63 +1,278 @@
 from .model import Notebook, Cell
 from .parser import NotebookParser
-from typing import Optional, Iterator, Union
+from typing import Optional, Iterator, Union, Generic, TypeVar, cast
 import requests
 import os
 import json
+from dataclasses import dataclass
 
 # TODO: Support caching
 # TODO: Support request ETag
 # TODO: Support listing notebooks (including private)
 # TODO: Support listing notebook revisions(including private)
 
+T = TypeVar("T")
 OBSERVABLE_API_KEY = "OBSERVABLE_API_KEY"
-CACHE: dict[str, str] = {}
+
+
+@dataclass
+class NotebookRef:
+    """An unambiguous reference to a notebook"""
+
+    id: str
+    version: int
+    username: Optional[str] = None
+    name: Optional[str] = None
+
+
+@dataclass
+class NotebookHeader:
+    """The parse result of a JavaScript header
+    for an observable notebook"""
+
+    id: Optional[str]
+    version: int
+    username: str
+    name: Optional[str]
+
+
+class Singleton(Generic[T]):
+    """Utility singleton"""
+
+    Instance: Optional[T] = None
+
+    @classmethod
+    def Get(cls):
+        if not cls.Instance:
+            cls.Instance = cls()
+        return cls.Instance
+
+    def __init__(self):
+        if not self.__class__.Instance:
+            self.__class__.Instance = self
+
+
+class ObservableAPI(Singleton["ObservableAPI"]):
+    """The baseline Observable API"""
+
+    def __init__(self, key: Optional[str] = None):
+        super().__init__
+        self.apikey: Optional[str] = key
+        self.cache: dict[str, str] = {}
+
+    def key(self, variable=OBSERVABLE_API_KEY) -> str:
+        return (os.getenv(variable) or "").strip("\n").strip()
+
+    def url(self, path: str) -> str:
+        return f"https://api.observablehq.com/{path}"
+
+    def request(self, url: str, key: Optional[str] = None) -> str:
+        if url in self.cache:
+            return self.cache[url]
+        api_key: str = key or self.key()
+        abs_url = self.url(url)
+        headers = {"Authorization": f"ApiKey {api_key}"} if api_key else {}
+        r = requests.get(abs_url, headers=headers)
+        if r.status_code >= 200 and r.status_code < 300:
+            self.cache[url] = r.text
+            return r.text
+        else:
+            raise RuntimeError(
+                f"Request to {url} failed with {r.status_code}: {r.text}"
+            )
+
+    def list(
+        self, path: str, key: Optional[str] = None, limit: int = 100
+    ) -> list[dict]:
+        res: dict[str, dict] = {}
+        before: Optional[str] = None
+        added: int = 1
+        # There's a limit of 30 results per page, so we use the "before" query
+        # parameter to iterate through the results.
+        while added != 0 and len(res) < limit:
+            added = 0
+            for item in json.loads(
+                self.request(
+                    (f"{path}?before={before}" if before else path),
+                    key or self.key(),
+                )
+            ):
+                nid = item["id"]
+                update_time = item["update_time"]
+                before = (
+                    update_time if before is None or update_time < before else before
+                )
+                if nid not in res:
+                    res[nid] = item
+                    added += 1
+        return [_ for _ in res.values()]
+
+
+class NotebookAPI(Singleton["NotebookAPI"]):
+    """Wraps the notebook/document Observable API"""
+
+    def __init__(self, api: Optional[ObservableAPI] = None):
+        super().__init__()
+        self.api: ObservableAPI = api or ObservableAPI.Get()
+        self.names: dict[str, str] = {}
+        self.latest: dict[str, int] = {}
+        self.ids: dict[str, str] = {}
+        self.resolved: dict[str, NotebookRef] = {}
+
+    def parseJSHeader(self, source: str) -> NotebookHeader:
+        meta = {
+            (k[3:].lower().strip().split()[0]): v.strip()
+            for k, v in (l.split(":", 1) for l in source.split("\n")[0:5])
+        }
+        # {'url': 'https://observablehq.com/d/XXXXXXXXXXXXXXXX', 'title':
+        # '000-Sitemap', 'author': 'Sébastien Pierre (@sebastien)', 'version':
+        # '38', 'runtime': ' 1'}
+        # -or-
+        # {'url': 'https://observablehq.com/@sebastien/boilerplate', 'title':
+        # 'Boilerplate', 'author': 'Sébastien Pierre (@sebastien)', 'version':
+        # '2234', 'runtime versi
+        return NotebookHeader(
+            id=meta["url"].rsplit("/d/", 1)[-1] if "/d/" in meta["url"] else None,
+            version=int(meta["version"]),
+            name=meta["url"].rsplit("/", 1)[-1] if "/@" in meta["url"] else None,
+            username=meta["author"].rsplit("(@", 1)[1].split(")")[0],
+        )
+
+    def resolve(
+        self, notebook: Union[NotebookRef, str], key: Optional[str] = None
+    ) -> NotebookRef:
+        """Resolves the given notebook name"""
+        # We reuse the resolution cache
+        if isinstance(notebook, NotebookRef):
+            return notebook
+        elif notebook in self.resolved:
+            return self.resolved[notebook]
+        name = Notebook.ParseName(notebook)
+        if not name:
+            raise ValueError(
+                f"Could not parse 'notebook', should be like '@user/notebook' or 'hexhash', got: {notebook}"
+            )
+        api_key = key or self.api.key()
+        rev = f"@{name.rev}" if name.rev is not None else ""
+        if Notebook.IsPrivate(name):
+            if not api_key:
+                raise RuntimeError(
+                    f"Missing Observable API key variable {OBSERVABLE_API_KEY}, visit https://observablehq.com/settings/api-keys to create one"
+                )
+            ref = f"d/{name.id}{rev}"
+            # https://api.observablehq.com/d/[NOTEBOOK_ID][@VERSION].[FORMAT]?v=3&api_key=xxxx
+            header = self.parseJSHeader(observable_request(f"{ref}.js", key))
+            assert name.id
+            document_id: str = header.id or name.id
+            document_latest: int = header.version
+            document_name: Optional[str] = header.name
+            document_user: str = header.username
+        elif resolved := self.resolved.get(f"@{name.username}/{name.name}"):
+            # In case the name was already resolved, we can return it right away
+            # and save a request.
+            document_id = resolved.id
+            document_latest = name.rev or resolved.version
+            document_name = name.name
+            assert name.username
+            document_user = name.username
+        else:
+            data = json.loads(
+                observable_request(f"document/@{name.username}/{name.name}{rev}", key),
+            )
+            # SEE: https://api.observablehq.com/document/@sebastien/boilerplate
+            document_id = str(data["id"])
+            document_latest = int(data["latest_version"])
+            document_name = str(data["slug"])
+            document_user = data["owner"]["login"]
+        assert document_id
+        self.latest[document_id] = document_latest
+        if document_name:
+            self.ids[document_name] = document_id
+            self.names[document_id] = document_name
+        res = NotebookRef(
+            id=document_id,
+            version=int(name.rev) if name.rev else document_latest,
+            username=document_user,
+            name=document_name,
+        )
+        self.resolved[notebook] = res
+        return res
+
+    # FIXME: We should add a resolver. And also the actual resolved ID may
+    # be different from the  ID that we requested. The Observable API returns
+    # the notebook AND its revision number.
+    def dependencies(
+        self, *notebook: Union[NotebookRef, str], key: Optional[str] = None
+    ) -> list[str]:
+        """Returns the list of all the dependencies, including transitive
+        dependencies from this notebook."""
+        loaded: dict[str, list[str]] = {}
+        to_process: list[NotebookRef] = [self.resolve(_, key) for _ in notebook]
+        while to_process:
+            nref = to_process.pop()
+            if nref.id in loaded:
+                continue
+            n = self.load(nref, key=key)
+            if n:
+                loaded[nref.id] = [_ for _ in n.imported]
+                to_process += [
+                    self.resolve(_) for _ in loaded[nref.id] if _ not in loaded
+                ]
+        return [_ for _ in loaded]
+
+    def get(
+        self,
+        notebook: Union[NotebookRef, str],
+        key: Optional[str] = None,
+    ) -> str:
+        """Downloads the given notebook, optionally using the given API key"""
+        ref = self.resolve(notebook)
+        api_key = key or self.api.key()
+        if not ref.name:
+            if not api_key:
+                raise RuntimeError(
+                    f"Missing Observable API key variable {OBSERVABLE_API_KEY}, visit https://observablehq.com/settings/api-keys to create one"
+                )
+            # https://api.observablehq.com/d/[NOTEBOOK_ID][@VERSION].[FORMAT]?v=3&api_key=xxxx
+            url = f"d/{ref.id}@{ref.version}.js"
+        else:
+            url = f"@{ref.username}/{ref.name}@{ref.version}.js"
+        return self.api.request(url, api_key)
+
+    def load(
+        self, notebook: Union[NotebookRef, str], key: Optional[str] = None
+    ) -> Optional[Notebook]:
+        """Gets and parses the given notebook"""
+        return self.parse(self.get(notebook, key=key))
+
+    def parse(self, content: str) -> Optional[Notebook]:
+        """Parses the given notebook text into a Notebook object"""
+        parser = NotebookParser()
+        for line in content.split("\n"):
+            parser.feed(f"{line}\n")
+        return parser.notebook
+
+
+# --
+# ## High-Level API
 
 
 def observable_key(variable=OBSERVABLE_API_KEY) -> str:
-    return (os.getenv(variable) or "").strip("\n").strip()
+    return ObservableAPI.Get().key(variable)
 
 
 def observable_url(path: str) -> str:
-    return f"https://api.observablehq.com/{path}"
+    return ObservableAPI.Get().url(path)
 
 
-def observable_request(url: str, key: Optional[Union[bool, str]] = True) -> str:
-    if url in CACHE:
-        return CACHE[url]
-    api_key = observable_key if key is True else key or ""
-    headers = {"Authorization": f"ApiKey {api_key}"} if api_key else {}
-    r = requests.get(observable_url(url), headers=headers)
-    if r.status_code >= 200 and r.status_code < 300:
-        CACHE[url] = r.text
-        return r.text
-    else:
-        raise RuntimeError(f"Request to {url} failed with {r.status_code}: {r.text}")
+def observable_request(url: str, key: Optional[str] = None) -> str:
+    return ObservableAPI.Get().request(url, key=key)
 
 
 def observable_list(
-    path: str, key: Optional[Union[bool, str]] = True, limit: int = 100
+    path: str, key: Optional[str] = None, limit: int = 100
 ) -> list[dict]:
-    res: dict[str, dict] = {}
-    before: Optional[str] = None
-    added: int = 1
-    # There's a limit of 30 results per page, so we use the "before" query
-    # parameter to iterate through the results.
-    while added != 0 and len(res) < limit:
-        added = 0
-        for item in json.loads(
-            observable_request(
-                (f"{path}?before={before}" if before else path),
-                key or observable_key(),
-            )
-        ):
-            nid = item["id"]
-            update_time = item["update_time"]
-            before = update_time if before is None or update_time < before else before
-            if nid not in res:
-                res[nid] = item
-                added += 1
-    return [_ for _ in res.values()]
+    return ObservableAPI.Get().list(path, key=key, limit=limit)
 
 
 def user_information(username: str):
@@ -83,55 +298,27 @@ def user_collection_notebooks(
     )
 
 
+def notebook_resolve(notebook: str, key: Optional[str] = None) -> NotebookRef:
+    return NotebookAPI.Get().resolve(notebook, key)
+
+
 def notebook_get(
     notebook: str,
     key: Optional[str] = None,
 ) -> str:
-    """Downloads the given notebook, optionally using the given API key"""
-    name = Notebook.ParseName(notebook)
-    if not name:
-        raise ValueError(
-            f"Could not parse 'notebook', should be like '@user/notebook' or 'hexhash', got: {notebook}"
-        )
-    api_key = key or observable_key()
-    rev = f"@{name.rev}" if name.rev is not None else ""
-    if Notebook.IsPrivate(name):
-        if not api_key:
-            raise RuntimeError(
-                f"Missing Observable API key variable {OBSERVABLE_API_KEY}, visit https://observablehq.com/settings/api-keys to create one"
-            )
-        # https://api.observablehq.com/d/[NOTEBOOK_ID][@VERSION].[FORMAT]?v=3&api_key=xxxx
-        url = f"d/{name.id}{rev}.js"
-    else:
-        url = f"@{name.username}/{name.name}{rev}.js?"
-    return observable_request(url, api_key)
+    return NotebookAPI.Get().get(notebook, key)
 
 
-def notebook_parse(content: str) -> Notebook:
-    """Parses the given notebook text into a Notebook object"""
-    parser = NotebookParser()
-    for line in content.split("\n"):
-        parser.feed(f"{line}\n")
-    return parser.notebook
+def notebook_parse(content: str) -> Optional[Notebook]:
+    return NotebookAPI.Get().parse(content)
 
 
-def notebook_load(notebook: str, key: Optional[str] = None) -> Notebook:
-    return notebook_parse(notebook_get(notebook, key=key))
+def notebook_load(notebook: str, key: Optional[str] = None) -> Optional[Notebook]:
+    return NotebookAPI.Get().load(notebook, key)
 
 
 def notebook_dependencies(*notebook: str, key: Optional[str] = None) -> list[str]:
-    """Returns the list of all the dependencies, including transitive
-    dependencies from this notebook."""
-    loaded: dict[str, list[str]] = {}
-    to_process: list[str] = [_ for _ in notebook]
-    while to_process:
-        nid = to_process.pop()
-        if nid in loaded:
-            continue
-        n = notebook_parse(notebook_get(nid, key=key))
-        loaded[nid] = [_ for _ in n.imported]
-        to_process += [_ for _ in loaded[nid] if _ not in loaded]
-    return [_ for _ in loaded]
+    return NotebookAPI.Get().dependencies(*notebook, key=key)
 
 
 def notebook_json(notebook: Notebook) -> str:
